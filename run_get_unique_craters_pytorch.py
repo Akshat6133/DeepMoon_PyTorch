5424a22370d02ac59038a9e9a2eafb149fcaccba
kprateek-dev <134163118+kprateek-dev@users.noreply.github.com>
2025-03-27 06:41:50 +0530

    Update and rename run_get_unique_craters.py to run_get_unique_craters_pytorch.py

--- run_get_unique_craters_pytorch.py
+++ run_get_unique_craters_pytorch.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python
 """Run/Obtain Unique Crater Distribution
 
     Execute extracting craters from model target predictions and filtering
     out duplicates.
     """
 import get_unique_craters as guc
-import sys
 import numpy as np
+import torch
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # Crater Parameters
 CP = {}
 
@@ -21,11 +21,11 @@ CP['datatype'] = 'test'
 CP['n_imgs'] = 30000
 
 # Hyperparameters
-CP['llt2'] = float(sys.argv[1])    # D_{L,L} from Silburt et. al (2017)
-CP['rt2'] = float(sys.argv[2])     # D_{R} from Silburt et. al (2017)
+CP['llt2'] = 2.6    # D_{L,L} from Silburt et. al (2017)
+CP['rt2'] = 1.8     # D_{R} from Silburt et. al (2017)
 
 # Location of model to generate predictions (if they don't exist yet)
-CP['dir_model'] = 'models/model.h5'
+CP['dir_model'] = 'models/model_default_params.pth'
 
 # Location of where hdf5 data images are stored
 CP['dir_data'] = 'catalogues/%s_images.hdf5' % CP['datatype']
@@ -36,6 +36,8 @@ CP['dir_preds'] = 'catalogues/%s_preds_n%d.hdf5' % (CP['datatype'],
 
 # Location of where final unique crater distribution will be stored
 CP['dir_result'] = 'catalogues/%s_craterdist.npy' % (CP['datatype'])
+
+CP['run_template_matching'] = False
 
 if __name__ == '__main__':
     craters_unique = np.empty([0, 3])
