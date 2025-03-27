b16710918239d84eaa1e0b6afd6148263917847a
kprateek-dev <134163118+kprateek-dev@users.noreply.github.com>
2025-03-27 06:15:42 +0530

    Update model_train_pytorch_transform_v2.py

--- model_train_pytorch_transform_v2.py
+++ model_train_pytorch_transform_v2.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 """Convolutional Neural Network Training Functions
 
 Functions for building and training a (UNET) Convolutional Neural Network on
@@ -18,28 +17,13 @@ from torchvision import datapoints
 import utils.template_match_target as tmt
 import utils.processing as proc
 
-import torch
-import torch.nn as nn
+
 import torch.nn.functional as F
 
-k2 = True  # Assuming Keras 2 is being used
-
-if k2:
-    from torch import cat as merge
-    from torch.nn import Conv2d as Convolution2D
-    from torch.nn import MaxPool2d as MaxPooling2D
-    from torch.nn import Upsample as UpSampling2D
-else:
-    raise ValueError("Only PyTorch 2 syntax is supported.")
-
-# No need to define a wrapper function for merging layers in PyTorch, as it's directly available as `torch.cat`.
-# Also, there's no need to define a wrapper function for Conv2D, MaxPooling2D, and UpSampling2D in PyTorch.
-# We directly use the corresponding modules from torch.nn.
-
-# You can adjust the values of k2 and the imports according to your actual use case and PyTorch version.
-
-
-#Using transforms.v2 instead of torchvision.transforms to randomly transform both image and mask in sync.
+torch.manual_seed(0)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+import matplotlib.pyplot as plt
+image_and_mask_path = ''
 
 ########################
 def get_param_i(param, i):
@@ -61,7 +45,9 @@ def get_param_i(param, i):
     else:
         return param[0]
 
-########################
+import torch
+
+
 class CraterDataset(Dataset):
     def __init__(self, data, target, transform=None):
         self.data = data
@@ -82,14 +68,10 @@ class CraterDataset(Dataset):
             image = image.to(device)
             mask = mask.unsqueeze(0).to(device)
         return image, mask
-########################
+
 def check_random_transform(self,image,mask,transform):
 
     mask = mask.squeeze(0)
-    print(image.shape)
-    print(mask.shape)
-    print(type(image))
-    print(type(mask))
     img = datapoints.Image(image)
     msk = datapoints.Mask(mask)
     image,mask = self.transform(img,msk)
@@ -97,15 +79,14 @@ def check_random_transform(self,image,mask,transform):
     mask = mask.to(device)
     image_cpu = image.cpu()
     mask_cpu = mask.cpu()
-    print(image_cpu.shape)
-    print(mask_cpu.shape)
     numpy_image= image_cpu.detach().numpy()
     numpy_mask = mask_cpu.detach().numpy()
 
     
     plt.imsave(image_and_mask_path+'_new_image.png',numpy_image.squeeze(0),cmap='gray')
     plt.imsave(image_and_mask_path+'_new_mask.png',numpy_mask,cmap='gray')
-########################
+
+
 transform = v2.Compose([
     v2.RandomHorizontalFlip(),
     v2.RandomVerticalFlip(),
@@ -113,21 +94,23 @@ transform = v2.Compose([
     v2.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
     v2.ToTensor(),
 ])
-########################
+
+
 def custom_image_generator(data, target, batch_size=32):
 
     dataset = CraterDataset(data, target, transform=transform)
     return DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
 ########################
 def get_metrics(data, craters, dim, model, beta=1):
-    """Function that prints pertinent metrics at the end of each epoch. 
+    """Function that prints pertinent metrics at the end of each epoch.
 
     Parameters
     ----------
     data : hdf5
         Input images.
     craters : hdf5
-        Pandas arrays of human-counted crater data. 
+        Pandas arrays of human-counted crater data.
     dim : int
         Dimension of input images (assumes square).
     model : PyTorch model object
@@ -140,37 +123,36 @@ def get_metrics(data, craters, dim, model, beta=1):
     # Get csvs of human-counted craters
     csvs = []
     minrad, maxrad, cutrad, n_csvs = 3, 50, 0.8, len(X)
-    diam = 'Diameter (pix)'
+    diam = "Diameter (pix)"
     for i in range(n_csvs):
         csv = craters[proc.get_id(i)]
         # remove small/large/half craters
         csv = csv[(csv[diam] < 2 * maxrad) & (csv[diam] > 2 * minrad)]
-        csv = csv[(csv['x'] + cutrad * csv[diam] / 2 <= dim)]
-        csv = csv[(csv['y'] + cutrad * csv[diam] / 2 <= dim)]
-        csv = csv[(csv['x'] - cutrad * csv[diam] / 2 > 0)]
-        csv = csv[(csv['y'] - cutrad * csv[diam] / 2 > 0)]
-        if len(csv) < 3:    # Exclude csvs with few craters
+        csv = csv[(csv["x"] + cutrad * csv[diam] / 2 <= dim)]
+        csv = csv[(csv["y"] + cutrad * csv[diam] / 2 <= dim)]
+        csv = csv[(csv["x"] - cutrad * csv[diam] / 2 > 0)]
+        csv = csv[(csv["y"] - cutrad * csv[diam] / 2 > 0)]
+        if len(csv) < 3:  # Exclude csvs with few craters
             csvs.append([-1])
         else:
-            csv_coords = np.asarray((csv['x'], csv['y'], csv[diam] / 2)).T
+            csv_coords = np.asarray((csv["x"], csv["y"], csv[diam] / 2)).T
             csvs.append(csv_coords)
 
-    # Calculate custom metrics
     print("")
     print("*********Custom Loss*********")
     recall, precision, fscore = [], [], []
     frac_new, frac_new2, maxrad = [], [], []
     err_lo, err_la, err_r = [], [], []
     frac_duplicates = []
-    model.eval()  # Set model to evaluation mode
-    with torch.no_grad():  # Disable gradient calculation
+    model.eval()  
+    with torch.no_grad():  
         preds = model(torch.tensor(X).float())
     for i in range(n_csvs):
         if len(csvs[i]) < 3:
             continue
-        (N_match, N_csv, N_detect, maxr,
-         elo, ela, er, frac_dupes) = tmt.template_match_t2c(preds[i].numpy(), csvs[i],
-                                                            rmv_oor_csvs=0)
+        (N_match, N_csv, N_detect, maxr, elo, ela, er, frac_dupes) = (
+            tmt.template_match_t2c(preds[i].numpy(), csvs[i], rmv_oor_csvs=0)
+        )
         if N_match > 0:
             p = float(N_match) / float(N_match + (N_detect - N_match))
             r = float(N_match) / float(N_csv)
@@ -189,55 +171,77 @@ def get_metrics(data, craters, dim, model, beta=1):
             err_r.append(er)
             frac_duplicates.append(frac_dupes)
         else:
-            print("skipping iteration %d,N_csv=%d,N_detect=%d,N_match=%d" %
-                  (i, N_csv, N_detect, N_match))
+            print(
+                "skipping iteration %d,N_csv=%d,N_detect=%d,N_match=%d"
+                % (i, N_csv, N_detect, N_match)
+            )
 
-    # Convert numpy arrays to PyTorch tensors
     X_tensor = torch.tensor(X).float()
     Y_tensor = torch.tensor(Y).float()
-
-    # Calculate binary cross-entropy loss
     criterion = nn.BCELoss()
     loss = criterion(model(X_tensor), Y_tensor)
     print("binary XE score = %f" % loss.item())
 
     if len(recall) > 3:
-        print("mean and std of N_match/N_csv (recall) = %f, %f" %
-              (np.mean(recall), np.std(recall)))
-        print("""mean and std of N_match/(N_match + (N_detect-N_match))
-              (precision) = %f, %f""" % (np.mean(precision), np.std(precision)))
-        print("mean and std of F_%d score = %f, %f" %
-              (beta, np.mean(fscore), np.std(fscore)))
-        print("""mean and std of (N_detect - N_match)/N_detect (fraction
-              of craters that are new) = %f, %f""" %
-              (np.mean(frac_new), np.std(frac_new)))
-        print("""mean and std of (N_detect - N_match)/N_csv (fraction of
-              "craters that are new, 2) = %f, %f""" %
-              (np.mean(frac_new2), np.std(frac_new2)))
-        print("median and IQR fractional longitude diff = %f, 25:%f, 75:%f" %
-              (np.median(err_lo), np.percentile(err_lo, 25),
-               np.percentile(err_lo, 75)))
-        print("median and IQR fractional latitude diff = %f, 25:%f, 75:%f" %
-              (np.median(err_la), np.percentile(err_la, 25),
-               np.percentile(err_la, 75)))
-        print("median and IQR fractional radius diff = %f, 25:%f, 75:%f" %
-              (np.median(err_r), np.percentile(err_r, 25),
-               np.percentile(err_r, 75)))
-        print("mean and std of frac_duplicates: %f, %f" %
-              (np.mean(frac_duplicates), np.std(frac_duplicates)))
-        print("""mean and std of maximum detected pixel radius in an image =
-              %f, %f""" % (np.mean(maxrad), np.std(maxrad)))
-        print("""absolute maximum detected pixel radius over all images =
-              %f""" % np.max(maxrad))
+        print(
+            "mean and std of N_match/N_csv (recall) = %f, %f"
+            % (np.mean(recall), np.std(recall))
+        )
+        print(
+            """mean and std of N_match/(N_match + (N_detect-N_match))
+              (precision) = %f, %f"""
+            % (np.mean(precision), np.std(precision))
+        )
+        print(
+            "mean and std of F_%d score = %f, %f"
+            % (beta, np.mean(fscore), np.std(fscore))
+        )
+        print(
+            """mean and std of (N_detect - N_match)/N_detect (fraction
+              of craters that are new) = %f, %f"""
+            % (np.mean(frac_new), np.std(frac_new))
+        )
+        print(
+            """mean and std of (N_detect - N_match)/N_csv (fraction of
+              "craters that are new, 2) = %f, %f"""
+            % (np.mean(frac_new2), np.std(frac_new2))
+        )
+        print(
+            "median and IQR fractional longitude diff = %f, 25:%f, 75:%f"
+            % (np.median(err_lo), np.percentile(err_lo, 25), np.percentile(err_lo, 75))
+        )
+        print(
+            "median and IQR fractional latitude diff = %f, 25:%f, 75:%f"
+            % (np.median(err_la), np.percentile(err_la, 25), np.percentile(err_la, 75))
+        )
+        print(
+            "median and IQR fractional radius diff = %f, 25:%f, 75:%f"
+            % (np.median(err_r), np.percentile(err_r, 25), np.percentile(err_r, 75))
+        )
+        print(
+            "mean and std of frac_duplicates: %f, %f"
+            % (np.mean(frac_duplicates), np.std(frac_duplicates))
+        )
+        print(
+            """mean and std of maximum detected pixel radius in an image =
+              %f, %f"""
+            % (np.mean(maxrad), np.std(maxrad))
+        )
+        print(
+            """absolute maximum detected pixel radius over all images =
+              %f"""
+            % np.max(maxrad)
+        )
         print("")
 
 
 ########################
+# import torch
 class UNet(nn.Module):
     def __init__(self, dim, n_filters, FL, init, drop, lmbda):
         super(UNet, self).__init__()
 
-        # Define layers
+       
         self.conv1 = nn.Conv2d(1, n_filters, FL, padding=FL // 2)
         self.conv2 = nn.Conv2d(n_filters, n_filters, FL, padding=FL // 2)
         self.conv3 = nn.Conv2d(n_filters, n_filters * 2, FL, padding=FL // 2)
@@ -248,30 +252,26 @@ class UNet(nn.Module):
         self.conv8 = nn.Conv2d(n_filters * 4, n_filters * 4, FL, padding=FL // 2)
         self.conv9 = nn.Conv2d(
             n_filters * 8, n_filters * 2, FL, padding=FL // 2
-        )  # Changed input channels
+        )  
         self.conv10 = nn.Conv2d(n_filters * 2, n_filters * 2, FL, padding=FL // 2)
         self.conv11 = nn.Conv2d(
             n_filters * 4, n_filters, FL, padding=FL // 2
-        )  # Changed input channels
+        )  
         self.conv12 = nn.Conv2d(n_filters, n_filters, FL, padding=FL // 2)
         self.conv13 = nn.Conv2d(
             n_filters * 2, n_filters, FL, padding=FL // 2
-        )  # Added to match concatenation
+        )  
         self.conv14 = nn.Conv2d(
             n_filters, n_filters, FL, padding=FL // 2
-        )  # Added to match concatenation
+        ) 
         self.final_conv = nn.Conv2d(n_filters, 1, 1, padding=0)
 
-        # Define pooling layers
         self.maxpool = nn.MaxPool2d(2, 2)
 
-        # Define upsampling layers
         self.upsample = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
 
-        # Define dropout layer
         self.dropout = nn.Dropout(drop)
 
-        # Initialization
         if init == "he_normal":
             for m in self.modules():
                 if isinstance(m, nn.Conv2d):
@@ -326,10 +326,11 @@ class UNet(nn.Module):
         u = self.final_conv(u)
         return torch.sigmoid(u)
 
+
 ########################
 def train_and_test_model(Data, Craters, MP, i_MP):
     """Function that trains, tests and saves the model, printing out metrics
-    after each model. 
+    after each model.
 
     Parameters
     ----------
@@ -342,63 +343,86 @@ def train_and_test_model(Data, Craters, MP, i_MP):
     i_MP : int
         Iteration number (when iterating over hypers).
     """
-    # Static params
-    dim, nb_epoch, bs = MP['dim'], MP['epochs'], MP['bs']
 
-    # Iterating params
-    FL = get_param_i(MP['filter_length'], i_MP)
-    learn_rate = get_param_i(MP['lr'], i_MP)
-    n_filters = get_param_i(MP['n_filters'], i_MP)
-    init = get_param_i(MP['init'], i_MP)
-    lmbda = get_param_i(MP['lambda'], i_MP)
-    drop = get_param_i(MP['dropout'], i_MP)
+    dim, nb_epoch, bs = MP["dim"], MP["epochs"], MP["bs"]
 
-    # Build model
-    model = UNet(dim, n_filters, FL, init, drop, lmbda)
 
-    # Define loss function and optimizer
+    FL = get_param_i(MP["filter_length"], i_MP)
+    learn_rate = get_param_i(MP["lr"], i_MP)
+    n_filters = get_param_i(MP["n_filters"], i_MP)
+    init = get_param_i(MP["init"], i_MP)
+    lmbda = get_param_i(MP["lambda"], i_MP)
+    drop = get_param_i(MP["dropout"], i_MP)
+
+
+    model = UNet(dim, n_filters, FL, init, drop, lmbda).to(device)
+    print(model)
+
     criterion = nn.BCELoss()
     optimizer = optim.Adam(model.parameters(), lr=learn_rate)
 
-    # Main loop
-    n_samples = MP['n_train']
+    from loguru import logger
+
+
+    n_samples = MP["n_train"]
+    runningLoss = []
     for nb in range(nb_epoch):
-        model.train()  # Set model to training mode
+        model.train()  
         running_loss = 0.0
-        for i, data in enumerate(custom_image_generator(Data['train'][0], Data['train'][1], batch_size=bs), 0):
+        for i, data in enumerate(
+            custom_image_generator(
+                Data["train"][0],  
+                Data["train"][1],  
+                batch_size=bs,
+            ),
+        ):
+            
             inputs, labels = data
-            optimizer.zero_grad()  # Zero the parameter gradients
-            outputs = model(inputs)  # Forward pass
-            loss = criterion(outputs, labels)  # Compute the loss
-            loss.backward()  # Backward pass
-            optimizer.step()  # Optimize
+
+            optimizer.zero_grad()  
+            outputs = model(inputs)  
+            loss = criterion(outputs, labels)  
+            loss.backward()  
+            optimizer.step()  
             running_loss += loss.item()
+            print(f'[Epoch {nb + 1}, Iteration {i + 1}] loss: {loss.item():.3f}')
 
-        # Print average loss
-        print('[Epoch %d] loss: %.3f' % (nb + 1, running_loss / n_samples))
 
-        # Evaluate model on validation set
-        model.eval()  # Set model to evaluation mode
-        with torch.no_grad():
+        model.eval() 
+        with torch.no_grad():  
             dev_loss = 0.0
-            for data in custom_image_generator(Data['dev'][0], Data['dev'][1], batch_size=bs):
+            for data in custom_image_generator(
+                Data["dev"][0], Data["dev"][1], batch_size=bs
+            ):
                 inputs, labels = data
                 outputs = model(inputs)
                 dev_loss += criterion(outputs, labels).item()
+            print("[Validation] loss: %.6f" % (dev_loss / n_samples))
 
-            # Print validation loss
-            print('[Validation] loss: %.3f' % (dev_loss / n_samples))
-
-    if MP['save_models'] == 1:
-        torch.save(model.state_dict(), MP['save_dir'])
+    if MP["save_models"] == 1:
+        torch.save(model.state_dict(), MP["save_dir"])
+        torch.save(model.state_dict(), MP["save_dir_2"])
 
     print("###################################")
     print("##########END_OF_RUN_INFO##########")
-    print("""learning_rate=%e, batch_size=%d, filter_length=%e, n_epoch=%d
+    print(
+        """learning_rate=%e, batch_size=%d, filter_length=%e, n_epoch=%d
           n_train=%d, img_dimensions=%d, init=%s, n_filters=%d, lambda=%e
-          dropout=%f""" % (learn_rate, bs, FL, nb_epoch, MP['n_train'],
-                           MP['dim'], init, n_filters, lmbda, drop))
-    get_metrics(Data['test'], Craters['test'], dim, model)
+          dropout=%f"""
+        % (
+            learn_rate,
+            bs,
+            FL,
+            nb_epoch,
+            MP["n_train"],
+            MP["dim"],
+            init,
+            n_filters,
+            lmbda,
+            drop,
+        )
+    )
+
     print("###################################")
     print("###################################")
 
@@ -412,54 +436,47 @@ def get_models(MP):
     MP : dict
         Model Parameters.
     """
-    dir = MP['dir']
-    n_train, n_dev, n_test = MP['n_train'], MP['n_dev'], MP['n_test']
+    dir = MP["dir"]
+    n_train, n_dev, n_test = MP["n_train"], MP["n_dev"], MP["n_test"]
 
     # Load data
-    train = h5py.File('%strain_images.hdf5' % dir, 'r')
-    dev = h5py.File('%sdev_images.hdf5' % dir, 'r')
-    test = h5py.File('%stest_images.hdf5' % dir, 'r')
+    train = h5py.File("%strain_images.hdf5" % dir, "r")
+    dev = h5py.File("%sdev_images.hdf5" % dir, "r")
+    test = h5py.File("%stest_images.hdf5" % dir, "r")
+    
     Data = {
-        'train': [torch.tensor(train['input_images'][:n_train]).float(),
-                  torch.tensor(train['target_masks'][:n_train]).float()],
-        'dev': [torch.tensor(dev['input_images'][:n_dev]).float(),
-                torch.tensor(dev['target_masks'][:n_dev]).float()],
-        'test': [torch.tensor(test['input_images'][:n_test]).float(),
-                 torch.tensor(test['target_masks'][:n_test]).float()]
+        "train": [
+            torch.tensor(train["input_images"][:n_train]).float().unsqueeze(1),
+            torch.tensor(train["target_masks"][:n_train]).float().unsqueeze(1),
+        ],
+        "dev": [
+            torch.tensor(dev["input_images"][:n_dev]).float().unsqueeze(1),
+            torch.tensor(dev["target_masks"][:n_dev]).float().unsqueeze(1),
+        ],
+        "test": [
+            torch.tensor(test["input_images"][:n_test]).float().unsqueeze(1),
+            torch.tensor(test["target_masks"][:n_test]).float().unsqueeze(1),
+        ],
     }
+
     train.close()
     dev.close()
     test.close()
 
-    # Rescale, normalize, add extra dim
     proc.preprocess(Data)
 
-    # Load ground-truth craters
     Craters = {
-        'train': pd.HDFStore('%strain_craters.hdf5' % dir, 'r'),
-        'dev': pd.HDFStore('%sdev_craters.hdf5' % dir, 'r'),
-        'test': pd.HDFStore('%stest_craters.hdf5' % dir, 'r')
+        "train": pd.HDFStore("%strain_craters.hdf5" % dir, "r"),
+        "dev": pd.HDFStore("%sdev_craters.hdf5" % dir, "r"),
+        "test": pd.HDFStore("%stest_craters.hdf5" % dir, "r"),
     }
 
-    # Iterate over parameters
-    for i in range(MP['N_runs']):
+
+    for i in range(
+        MP["N_runs"]
+
+        ):
         train_and_test_model(Data, Craters, MP, i)
 
 
-
-
-# from keras.callbacks import Callback
-
-# class NaNCheck(Callback):
-#     def on_batch_end(self, batch, logs=None):
-#         loss = logs.get('loss')
-#         if loss is not None and np.isnan(loss):
-#             print("Training halted due to NaN loss.")
-#             raise ValueError("Training halted due to NaN loss.")
-
-# from tensorflow.keras.callbacks import TensorBoard
-
-# tensorboard_callback = TensorBoard(log_dir='./logs')
-
-# model.fit(X_train, y_train, batch_size=32, epochs=50, validation_data=(X_val, y_val), callbacks=[tensorboard_callback])
 
